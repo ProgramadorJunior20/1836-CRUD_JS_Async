cf82410242e32ccb363b417d22f98ec001d186e8
Jefferson Murillo <development@eticsas.co>
2022-11-11 16:08:07 -0500

    Clase03 terminada correctamente

--- controllers/client.controler.js
+++ controllers/client.controler.js
@@ -1,8 +1,7 @@
 import { clientServices } from "../service/client-service.js";
 
-console.log(clientServices);
 
-const crearNuevaLinea = (nombre, email) => {
+const crearNuevaLinea = (nombre, email, id) => {
   const linea = document.createElement("tr");
   const contenido = `
       <td class="td" data-td>
@@ -20,7 +19,7 @@ const crearNuevaLinea = (nombre, email) => {
             </a>
           </li>
           <li>
-            <button class="simple-button simple-button--delete" type="button">
+            <button class="simple-button simple-button--delete" type="button" id="${id}">
               Eliminar
             </button>
           </li>
@@ -28,6 +27,17 @@ const crearNuevaLinea = (nombre, email) => {
       </td>
     `;
   linea.innerHTML = contenido;
+  const btn = linea.querySelector("button");
+  btn.addEventListener("click", () => {
+    const id = btn.id;
+    clientServices
+      .deleteClient(id)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => alert("Ocurrio un error"));
+  });
+
   return linea;
 };
 
@@ -36,10 +46,10 @@ const table = document.querySelector("[data-table]");
 clientServices
   .listaClientes()
   .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+    data.forEach(({ nombre, email, id }) => {
+      const nuevaLinea = crearNuevaLinea(nombre, email, id);
       table.appendChild(nuevaLinea);
     });
-    console.log(data);
+    /* console.log(data); */
   })
   .catch((e) => alert("Ocurrio un error..."));
